f3ec3dcc875a61dc014a9603674fe5ca82f590b3
DavidJGapCR <djimenez@growthaccelerationpartners.com>
2021-02-16 15:40:42 -0600

    Combines AssessmentDim view and ObjectiveAssessmentDim view into AssessmentFact view.

---
+++ src/EdFi.AnalyticsMiddleTier.Tests/Classes/AssessmentFact.cs
@@ -0,0 +1,32 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EdFi.AnalyticsMiddleTier.Tests.Classes
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class AssessmentFact
+    {
+        public string AssessmentKey { get; set; }
+        public string Namespace { get; set; }
+        public string Title { get; set; }
+        public int Version { get; set; }
+        public string Category { get; set; }
+        public string AssessedGradeLevel { get; set; }
+        public string AcademicSubject { get; set; }
+        public string AssessmentMinScore { get; set; }
+        public string AssessmentMaxScore { get; set; }
+        //
+        public string ObjectiveAssessmentKey { get; set; }
+        public string IdentificationCode { get; set; }
+        public string ParentObjectiveAssessmentKey { get; set; }
+        public string ObjectiveAssessmentDescription { get; set; }
+        public string PercentOfAssessment { get; set; }
+        public string ObjectiveAssessmentScoreMinScore { get; set; }
+        public string ObjectiveAssessmentScoreMaxScore { get; set; }
+    }
+}
