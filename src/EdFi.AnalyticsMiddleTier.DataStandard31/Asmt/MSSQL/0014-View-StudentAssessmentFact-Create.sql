31b783c57311ddf423f38a05edb5ca533df82684
Rubén Solano Michel <ruben@nearshoredevs.com>
2021-02-26 11:19:16 -0600

    fixing sql scripts

--- src/EdFi.AnalyticsMiddleTier.DataStandard31/Asmt/MSSQL/0014-View-StudentAssessmentFact-Create.sql
+++ src/EdFi.AnalyticsMiddleTier.DataStandard31/Asmt/MSSQL/0014-View-StudentAssessmentFact-Create.sql
@@ -13,13 +13,13 @@ CREATE VIEW analytics.asmt_StudentAssessmentFact AS
 
 	SELECT
         CONCAT(StudentAssessment.AssessmentIdentifier, '-', StudentAssessment.Namespace, '-', StudentAssessment.StudentAssessmentIdentifier, '-', StudentAssessment.StudentUSI) AS StudentAssessmentKey,
-		CASE WHEN SASOA.StudentUSI = '' 
-                THEN NULL 
+		CASE WHEN SASOA.StudentUSI IS NULL
+                THEN ''
              ELSE 
                 CONCAT(SASOA.StudentUSI, '-', SASOA.IdentificationCode, '-', SASOA.AssessmentIdentifier, '-', SASOA.StudentAssessmentIdentifier, '-', SASOA.Namespace) 
         END AS StudentObjectiveAssessmentKey,
-        CASE WHEN SASOA.AssessmentIdentifier = ''
-                THEN NULL 
+        CASE WHEN SASOA.AssessmentIdentifier IS NULL
+                THEN ''
              ELSE 
                 CONCAT(SASOA.AssessmentIdentifier, '-', SASOA.IdentificationCode, '-', SASOA.Namespace) 
         END AS ObjectiveAssessmentKey,
