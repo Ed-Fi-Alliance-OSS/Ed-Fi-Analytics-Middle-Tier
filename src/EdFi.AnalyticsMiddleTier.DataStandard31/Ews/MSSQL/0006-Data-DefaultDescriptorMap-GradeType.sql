a03d2ee87013ec41342d4642ba2f16fe1009388b
José Alberto Leiva Obando <56046999+jleiva-gap@users.noreply.github.com>
2021-04-13 09:11:26 -0600

    [BIA-616]  Added GradeType to analytics.ews_StudentSectionGradeFact . (#35)

---
+++ src/EdFi.AnalyticsMiddleTier.DataStandard31/Ews/MSSQL/0006-Data-DefaultDescriptorMap-GradeType.sql
@@ -0,0 +1,72 @@
+﻿-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+WITH gradingTypeSemester
+     AS (SELECT 
+                DescriptorConstant.DescriptorConstantId, 
+                d.DescriptorId
+         FROM 
+              analytics_config.DescriptorConstant
+         CROSS JOIN
+             ( SELECT 
+                      DescriptorId
+               FROM 
+                    edfi.GradeTypeDescriptor
+               INNER JOIN
+                   edfi.Descriptor ON
+                       GradeTypeDescriptor.GradeTypeDescriptorId = Descriptor.DescriptorId
+               WHERE
+                       Descriptor.CodeValue = 'Semester'
+                       AND Namespace LIKE '%/GradeTypeDescriptor'
+             ) AS d
+         WHERE
+                 DescriptorConstant.ConstantName = 'GradeType.Semester'),
+     gradingTypeFinal
+     AS (SELECT 
+                DescriptorConstant.DescriptorConstantId, 
+                d.DescriptorId
+         FROM 
+              analytics_config.DescriptorConstant
+         CROSS JOIN
+             ( SELECT 
+                      DescriptorId
+               FROM 
+                    edfi.GradeTypeDescriptor
+               INNER JOIN
+                   edfi.Descriptor ON
+                       GradeTypeDescriptor.GradeTypeDescriptorId = Descriptor.DescriptorId
+               WHERE
+                       Descriptor.CodeValue = 'Final'
+                       AND Namespace LIKE '%/GradeTypeDescriptor'
+             ) AS d
+         WHERE
+                 DescriptorConstant.ConstantName = 'GradeType.Final')
+     MERGE INTO analytics_config.DescriptorMap AS Target
+     USING
+           ( SELECT 
+                    *
+             FROM 
+                  gradingTypeSemester
+             UNION ALL
+             SELECT 
+                    *
+             FROM 
+                  gradingTypeFinal
+           ) AS Source(DescriptorConstantId, DescriptorId)
+     ON
+             TARGET.DescriptorConstantId = Source.DescriptorConstantId
+     WHEN NOT MATCHED BY TARGET
+         THEN
+           INSERT(
+                  DescriptorConstantId, 
+                  DescriptorId, 
+                  CreateDate)
+           VALUES(Source.DescriptorConstantId
+,                 Source.DescriptorId
+,                 GETDATE()
+                 )
+     OUTPUT 
+            $action, 
+            inserted.*;
