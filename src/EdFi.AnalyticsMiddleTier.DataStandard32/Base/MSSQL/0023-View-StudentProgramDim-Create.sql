b547a4323aa7c1c9c72fc097db566d3651678e91
Ben Tillman <ben@headspring.com>
2021-05-11 10:52:45 -0500

    Add new StudentProgramDim view for sql and postgres

---
+++ src/EdFi.AnalyticsMiddleTier.DataStandard32/Base/MSSQL/0023-View-StudentProgramDim-Create.sql
@@ -0,0 +1,34 @@
+﻿-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+
+IF EXISTS
+(
+    SELECT 1
+    FROM [INFORMATION_SCHEMA].[VIEWS]
+    WHERE [TABLE_SCHEMA] = 'analytics'
+          AND [TABLE_NAME] = 'StudentProgramDim'
+)
+    BEGIN
+        DROP VIEW [analytics].[StudentProgramDim];
+END;
+GO
+CREATE VIEW [analytics].[StudentProgramDim]
+AS
+    SELECT [BeginDate],program.[EducationOrganizationId],program.[ProgramName],program.[ProgramTypeDescriptorId],student.[StudentUSI],
+    CONCAT (Student.StudentUniqueId,'-',StudentSchoolAssociation.SchoolId) AS StudentSchoolKey
+    FROM [edfi].[Program] Program
+    INNER JOIN
+        [edfi].[StudentProgramAssociation] StudentProgram
+        ON StudentProgram.ProgramName = Program.ProgramName
+        AND StudentProgram.ProgramTypeDescriptorId = Program.ProgramTypeDescriptorId
+        AND StudentProgram.ProgramEducationOrganizationId = Program.EducationOrganizationId
+    INNER JOIN 
+        edfi.Student Student
+        ON StudentProgram.StudentUSI = Student.StudentUSI
+    INNER JOIN 
+        edfi.StudentSchoolAssociation
+        ON Student.StudentUSI = edfi.StudentSchoolAssociation.StudentUSI
+GO
