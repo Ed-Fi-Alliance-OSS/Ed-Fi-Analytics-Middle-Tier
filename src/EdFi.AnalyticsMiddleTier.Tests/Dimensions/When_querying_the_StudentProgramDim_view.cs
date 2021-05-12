67c5384faa8182f9e3808894be2494f97599a2e3
Ben Tillman <ben@headspring.com>
2021-05-12 15:48:20 -0500

    Setup tests cases with basic tests for new StudentProgramDim view

---
+++ src/EdFi.AnalyticsMiddleTier.Tests/Dimensions/When_querying_the_StudentProgramDim_view.cs
@@ -0,0 +1,53 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using EdFi.AnalyticsMiddleTier.Tests.Classes;
+using NUnit.Framework;
+using Shouldly;
+
+namespace EdFi.AnalyticsMiddleTier.Tests.Dimensions
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public abstract class When_querying_the_StudentProgramDim_view : When_querying_a_view
+    {
+
+        protected const string TestCasesFolder = "TestCases.StudentProgramDim";
+
+        protected (bool success, string errorMessage) Result;
+
+        [OneTimeSetUp]
+        public void PrepareDatabase()
+        {
+            DataStandard.PrepareDatabase();
+        }
+
+        [OneTimeSetUp]
+        public void Act()
+        {
+            Result = DataStandard.LoadTestCaseData<StudentSchoolDim>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0000_StudentProgramDim_Data_Load.xml");
+            Result.success.ShouldBeTrue($"Error while loading data: '{Result.errorMessage}'");
+
+            Result = DataStandard.Install(10);
+            Result.success.ShouldBeTrue($"Error while installing Base: '{Result.errorMessage}'");
+        }
+
+        [Test]
+        public void Then_view_should_match_column_dictionary()
+        {
+            (bool success, string errorMessage) testResult = DataStandard.RunTestCase<TableColumns>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0001_StudentProgramDim_should_match_column_dictionary.xml");
+            testResult.success.ShouldBe(true, testResult.errorMessage);
+        }
+
+        public class Given_student_100021874
+            : When_querying_a_view
+        {
+            private const string _caseIdentifier = "100021874";
+            public Given_student_100021874(TestHarness dataStandard) => SetDataStandard(dataStandard);
+
+            [Test]
+            public void Then_should_return_one_record()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{_caseIdentifier}_should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+        }
+    }
+}
