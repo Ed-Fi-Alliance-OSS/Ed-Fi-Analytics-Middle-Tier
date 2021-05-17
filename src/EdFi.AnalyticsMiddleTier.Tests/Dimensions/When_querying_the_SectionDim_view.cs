a1c9e3fe2fc64668da253aa16496c55fbf4efaa9
Ruben Solano <64993785+NSD-Ruben@users.noreply.github.com>
2021-05-17 12:07:20 -0500

    [BIA-675] Adding view to AMT project and test cases for it (#43)

---
+++ src/EdFi.AnalyticsMiddleTier.Tests/Dimensions/When_querying_the_SectionDim_view.cs
@@ -0,0 +1,226 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.Diagnostics.CodeAnalysis;
+using EdFi.AnalyticsMiddleTier.Common;
+using EdFi.AnalyticsMiddleTier.Tests.Classes;
+using NUnit.Framework;
+using Shouldly;
+using CommonLib = EdFi.AnalyticsMiddleTier.Common;
+
+namespace EdFi.AnalyticsMiddleTier.Tests.Dimensions
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public abstract class When_querying_the_SectionDim_view_base : When_querying_a_view
+    {
+        protected const string TestCasesFolder = "TestCases.SectionDim";
+
+        protected (bool success, string errorMessage) Result;
+
+        [OneTimeSetUp]
+        public void PrepareDatabase()
+        {
+            DataStandard.PrepareDatabase();
+        }
+
+        [OneTimeSetUp]
+        public void Act()
+        {
+            Result = DataStandard.LoadTestCaseData<SectionDim>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0000_SectionDim_Data_Load.xml");
+            Result.success.ShouldBeTrue($"Error while loading data: '{Result.errorMessage}'");
+
+            // Install the default map so that we can test for Section address
+            Result = DataStandard.Install(10);
+            Result.success.ShouldBeTrue($"Error while installing Base: '{Result.errorMessage}'");
+        }
+
+        [Test]
+        public void Then_view_should_match_column_dictionary()
+        {
+            (bool success, string errorMessage) testResult = DataStandard.RunTestCase<TableColumns>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0001_SectionDim_should_match_column_dictionary.xml");
+            testResult.success.ShouldBe(true, testResult.errorMessage);
+        }
+
+        [Test]
+        public void Then_should_have_SectionKey()
+        {
+            (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.YTAR11_should_have_SectionKey.xml");
+            testResult.success.ShouldBe(true, testResult.errorMessage);
+        }
+
+        [Test]
+        public void Then_should_have_SectionName()
+        {
+            (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.YTAR11_should_have_SectionName.xml");
+            testResult.success.ShouldBe(true, testResult.errorMessage);
+        }
+
+        [Test]
+        public void Then_should_have_SessionName()
+        {
+            (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.YTAR11_should_have_SessionName.xml");
+            testResult.success.ShouldBe(true, testResult.errorMessage);
+        }
+
+        [Test]
+        public void Then_should_have_EducationalEnvironmentDescriptor()
+        {
+            (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.YTAR11_should_have_EducationalEnvironmentDescriptor.xml");
+            testResult.success.ShouldBe(true, testResult.errorMessage);
+        }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public class When_querying_the_SectionDim_view
+            : When_querying_the_SectionDim_view_base
+        {
+            public When_querying_the_SectionDim_view(TestHarness dataStandard) => SetDataStandard(dataStandard);
+
+            private const string _caseIdentifier = "YTAR11";
+
+            [Test]
+            public void Then_should_return_one_record()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{_caseIdentifier}_should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_SchoolKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{_caseIdentifier}_should_have_SchoolKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_LocalCourseCode()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{_caseIdentifier}_should_have_LocalCourseCode.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_SchoolYear()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{_caseIdentifier}_should_have_SchoolYear.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_LocalEducationAgencyId()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{_caseIdentifier}_should_have_LocalEducationAgencyId.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+        }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public class When_querying_the_SectionDim_V2_view
+            : When_querying_the_SectionDim_view_base
+        {
+            public When_querying_the_SectionDim_V2_view(TestHarness dataStandard) => SetDataStandard(dataStandard);
+
+            [SetUp]
+            public void SetUp()
+            {
+                if (DataStandard.DataStandardVersion.Equals(CommonLib.DataStandard.Ds31) || DataStandard.DataStandardVersion.Equals(CommonLib.DataStandard.Ds32))
+                {
+                    Assert.Ignore($"No validation needed because the query is different. ({DataStandard.DataStandardVersion.ToString()})");
+                }
+            }
+
+            [Test]
+            public void Then_should_return_one_record()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0002_Should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_return_one_record_2()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0003_Should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+        }
+
+        public class When_querying_the_SectionDim_V3_view
+            : When_querying_the_SectionDim_view_base
+        {
+            public When_querying_the_SectionDim_V3_view(TestHarness dataStandard) => SetDataStandard(dataStandard);
+
+            [SetUp]
+            public void SetUp()
+            {
+                if (DataStandard.DataStandardVersion.Equals(CommonLib.DataStandard.Ds2))
+                {
+                    Assert.Ignore($"No validation needed because the query is different. ({DataStandard.DataStandardVersion.ToString()})");
+                }
+            }
+
+            [Test]
+            public void Then_should_return_one_record()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0002_Should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_return_one_record_2()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0003_Should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_return_no_records()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0004_Should_return_no_records.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_return_one_record_3()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0005_Should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_return_one_record_4()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0006_Should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_return_one_record_5()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0007_Should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_return_one_record_6()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0008_Should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_return_one_record_7()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0009_Should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_return_one_record_8()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0010_Should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+        }
+    }
+}
