5f542ed9c9c44ee702198bc73e7cb1dc2d397e01
José Alberto Leiva Obando <56046999+jleiva-gap@users.noreply.github.com>
2021-06-25 15:37:48 -0600

    [BIA-737] Create equity_FeederSchoolDim (#53)

---
+++ src/EdFi.AnalyticsMiddleTier.DataStandard32/Equity/PostgreSQL/0002-View-FeederSchoolDim-Create.sql
@@ -0,0 +1,25 @@
+﻿-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+CREATE OR ALTER VIEW analytics.equity_FeederSchoolDim
+AS
+SELECT 
+    CONCAT(FeederSchoolAssociation.SchoolId,'-',FeederSchoolAssociation.FeederSchoolId) AS FeederSchoolUniqueKey
+    ,FeederSchoolAssociation.SchoolId AS SchoolKey
+    ,FeederSchoolAssociation.FeederSchoolId AS FeederSchoolKey
+    ,EducationOrganization.NameOfInstitution AS FeederSchoolName
+    ,(
+        SELECT MAX(MaxLastModifiedDate)
+        FROM (
+            VALUES (FeederSchoolAssociation.LastModifiedDate)
+                ,(EducationOrganization.LastModifiedDate)
+            ) AS VALUE(MaxLastModifiedDate)
+        ) AS LastModifiedDate
+FROM edfi.FeederSchoolAssociation
+INNER JOIN edfi.School ON FeederSchoolAssociation.SchoolId = School.SchoolId
+INNER JOIN edfi.School FeederSchool ON FeederSchoolAssociation.FeederSchoolId = FeederSchool.SchoolId
+INNER JOIN edfi.EducationOrganization ON FeederSchool.SchoolId = EducationOrganization.EducationOrganizationId
+WHERE (
+        FeederSchoolAssociation.EndDate IS NULL
+        OR FeederSchoolAssociation.EndDate >= NOW());
---
+++ src/EdFi.AnalyticsMiddleTier.Tests/Dimensions/When_querying_the_FeederSchoolDim_view.cs
@@ -0,0 +1,203 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using EdFi.AnalyticsMiddleTier.Tests.Classes;
+using NUnit.Framework;
+using Shouldly;
+using System.Diagnostics.CodeAnalysis;
+using EdFi.AnalyticsMiddleTier.Common;
+using CommonLib = EdFi.AnalyticsMiddleTier.Common;
+
+namespace EdFi.AnalyticsMiddleTier.Tests.Dimensions
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public abstract class When_querying_the_FeederSchoolDim_view : When_querying_a_view
+    {
+        protected const string TestCasesFolder = "TestCases.FeederSchoolDim";
+
+        protected (bool success, string errorMessage) Result;
+
+        [OneTimeSetUp]
+        public void PrepareDatabase()
+        {
+            if (DataStandard.DataStandardVersion.Equals(CommonLib.DataStandard.Ds2))
+            {
+                Assert.Ignore($"The FeederSchoolDim view does not exist in this version of the Data Standard. ({DataStandard.DataStandardVersion.ToString()})");
+            }
+            else
+            {
+                DataStandard.PrepareDatabase();
+            }
+        }
+
+        [OneTimeSetUp]
+        public void Act()
+        {
+            if (DataStandard.DataStandardVersion.Equals(CommonLib.DataStandard.Ds2))
+            {
+                Assert.Ignore(
+                    $"The FeederSchoolDim view does not exist in this version of the Data Standard. ({DataStandard.DataStandardVersion.ToString()})");
+            }
+            else
+            {
+                Result = DataStandard.LoadTestCaseData<UserDim>($"{TestCasesFolder}.0000_FeederSchoolDim_Data_Load.xml");
+                Result.success.ShouldBeTrue($"Error while loading data: '{Result.errorMessage}'");
+
+                Result = DataStandard.Install(10, Component.Equity);
+                Result.success.ShouldBeTrue($"Error while installing Base and Equity: '{Result.errorMessage}'");
+            }
+        }
+
+        [Test]
+        public void Then_view_should_match_column_dictionary()
+        {
+            (bool success, string errorMessage) testResult = DataStandard.RunTestCase<TableColumns>($"{TestCasesFolder}.0001_FeederSchoolDim_should_match_column_dictionary.xml");
+            testResult.success.ShouldBe(true, testResult.errorMessage);
+        }
+
+        public class Given_feederSchoolDim_850786060_628530
+        : When_querying_the_FeederSchoolDim_view
+        {
+            public Given_feederSchoolDim_850786060_628530(TestHarness dataStandard) => SetDataStandard(dataStandard);
+
+            private const string _caseIdentifier = "850786060_628530";
+
+            [SetUp]
+            public void IgnoreTestCase()
+            {
+                if (DataStandard.DataStandardVersion.Equals(CommonLib.DataStandard.Ds2))
+                {
+                    Assert.Ignore(
+                        $"The FeederSchoolDim view does not exist in this version of the Data Standard. ({DataStandard.DataStandardVersion.ToString()})");
+                }
+            }
+
+            [Test]
+            public void Then_should_return_one_record()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{_caseIdentifier}_should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_FeederSchoolKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<FeederSchoolDim>($"{TestCasesFolder}.{_caseIdentifier}_should_have_FeederSchoolKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_FeederSchoolName()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<FeederSchoolDim>($"{TestCasesFolder}.{_caseIdentifier}_should_have_FeederSchoolName.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_FeederSchoolUniqueKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<FeederSchoolDim>($"{TestCasesFolder}.{_caseIdentifier}_should_have_FeederSchoolUniqueKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_LastModifiedDate()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<FeederSchoolDim>($"{TestCasesFolder}.{_caseIdentifier}_should_have_LastModifiedDate.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_SchoolKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<StudentDisciplineActionDim>($"{TestCasesFolder}.{_caseIdentifier}_should_have_SchoolKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+        }
+        public class Given_feederSchoolDim_867530_850786060
+        : When_querying_the_FeederSchoolDim_view
+        {
+            public Given_feederSchoolDim_867530_850786060(TestHarness dataStandard) => SetDataStandard(dataStandard);
+
+            private const string _caseIdentifier = "867530_850786060";
+
+            [SetUp]
+            public void IgnoreTestCase()
+            {
+                if (DataStandard.DataStandardVersion.Equals(CommonLib.DataStandard.Ds2))
+                {
+                    Assert.Ignore(
+                        $"The FeederSchoolDim view does not exist in this version of the Data Standard. ({DataStandard.DataStandardVersion.ToString()})");
+                }
+            }
+
+            [Test]
+            public void Then_should_return_one_record()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{_caseIdentifier}_should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_FeederSchoolKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<FeederSchoolDim>($"{TestCasesFolder}.{_caseIdentifier}_should_have_FeederSchoolKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_FeederSchoolName()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<FeederSchoolDim>($"{TestCasesFolder}.{_caseIdentifier}_should_have_FeederSchoolName.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_FeederSchoolUniqueKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<FeederSchoolDim>($"{TestCasesFolder}.{_caseIdentifier}_should_have_FeederSchoolUniqueKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_LastModifiedDate()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<FeederSchoolDim>($"{TestCasesFolder}.{_caseIdentifier}_should_have_LastModifiedDate.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_SchoolKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<StudentDisciplineActionDim>($"{TestCasesFolder}.{_caseIdentifier}_should_have_SchoolKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+        }
+        public class Given_feederSchoolDim_628530_867530
+        : When_querying_the_FeederSchoolDim_view
+        {
+            public Given_feederSchoolDim_628530_867530(TestHarness dataStandard) => SetDataStandard(dataStandard);
+
+            private const string _caseIdentifier = "628530_867530";
+
+            [SetUp]
+            public void IgnoreTestCase()
+            {
+                if (DataStandard.DataStandardVersion.Equals(CommonLib.DataStandard.Ds2))
+                {
+                    Assert.Ignore(
+                        $"The FeederSchoolDim view does not exist in this version of the Data Standard. ({DataStandard.DataStandardVersion.ToString()})");
+                }
+            }
+
+            [Test]
+            public void Then_should_not_return_any_record()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{_caseIdentifier}_should_not_return_any_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+        }
+    }
+}
