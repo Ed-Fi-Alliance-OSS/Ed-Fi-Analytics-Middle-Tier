835b81219e263a40b9301c9fced187f22d28b836
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2021-07-06 08:40:45 -0600

    Update script format

--- src/EdFi.AnalyticsMiddleTier.DataStandard31/Equity/MSSQL/0003-View-StudentDisciplineActionDim-Create.sql
+++ src/EdFi.AnalyticsMiddleTier.DataStandard31/Equity/MSSQL/0003-View-StudentDisciplineActionDim-Create.sql
@@ -2,9 +2,14 @@
 -- Licensed to the Ed-Fi Alliance under one or more agreements.
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
-IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.VIEWS WHERE TABLE_SCHEMA = 'analytics' AND TABLE_NAME = 'equity_StudentDisciplineActionDim')
+IF EXISTS (
+        SELECT 1
+        FROM INFORMATION_SCHEMA.VIEWS
+        WHERE TABLE_SCHEMA = 'analytics'
+            AND TABLE_NAME = 'equity_StudentDisciplineActionDim'
+        )
 BEGIN
-	DROP VIEW analytics.equity_StudentDisciplineActionDim
+    DROP VIEW analytics.equity_StudentDisciplineActionDim
 END
 GO
 
@@ -13,7 +18,7 @@ AS
 SELECT DISTINCT CONCAT (
         DisciplineAction.DisciplineActionIdentifier
         ,'-'
-        ,CONVERT(nvarchar(10),DisciplineAction.DisciplineDate,112)
+        ,CONVERT(NVARCHAR(10), DisciplineAction.DisciplineDate, 112)
         ,'-'
         ,Student.StudentUniqueId
         ,'-'
@@ -24,11 +29,11 @@ SELECT DISTINCT CONCAT (
         ,'-'
         ,StudentSchoolAssociation.SchoolId
         ) AS StudentSchoolKey
-    ,CONVERT(nvarchar(10),DisciplineAction.DisciplineDate,112) as DisciplineDateKey
+    ,CONVERT(NVARCHAR(10), DisciplineAction.DisciplineDate, 112) AS DisciplineDateKey
     ,Student.StudentUniqueId AS StudentKey
     ,StudentSchoolAssociation.SchoolId AS SchoolKey
     ,Descriptor.Description AS DisciplineActionDescription
-    ,COALESCE(Staff.StaffUniqueId, '') AS  UserKey
+    ,COALESCE(Staff.StaffUniqueId, '') AS UserKey
     ,(
         SELECT MAX(MaxLastModifiedDate)
         FROM (
@@ -38,19 +43,26 @@ SELECT DISTINCT CONCAT (
             ) AS VALUE(MaxLastModifiedDate)
         ) AS LastModifiedDate
 FROM edfi.DisciplineAction
-INNER JOIN edfi.Student ON DisciplineAction.StudentUSI = Student.StudentUSI
-INNER JOIN edfi.StudentSchoolAssociation ON Student.StudentUSI = StudentSchoolAssociation.StudentUSI
-INNER JOIN edfi.DisciplineActionDiscipline ON DisciplineAction.DisciplineActionIdentifier = DisciplineActionDiscipline.DisciplineActionIdentifier
-    AND DisciplineAction.DisciplineDate = DisciplineActionDiscipline.DisciplineDate
-    AND DisciplineAction.StudentUSI = DisciplineActionDiscipline.StudentUSI
-INNER JOIN edfi.Descriptor ON DisciplineActionDiscipline.DisciplineDescriptorId = Descriptor.DescriptorId
-LEFT JOIN edfi.DisciplineActionStaff ON DisciplineAction.DisciplineActionIdentifier = DisciplineActionStaff.DisciplineActionIdentifier
-    AND DisciplineAction.DisciplineDate = DisciplineActionStaff.DisciplineDate
-    AND DisciplineAction.StudentUSI = DisciplineActionStaff.StudentUSI
-LEFT JOIN edfi.Staff ON DisciplineActionStaff.StaffUSI = Staff.StaffUSI
- WHERE(
+INNER JOIN edfi.Student
+    ON DisciplineAction.StudentUSI = Student.StudentUSI
+INNER JOIN edfi.StudentSchoolAssociation
+    ON Student.StudentUSI = StudentSchoolAssociation.StudentUSI
+INNER JOIN edfi.DisciplineActionDiscipline
+    ON DisciplineAction.DisciplineActionIdentifier = DisciplineActionDiscipline.DisciplineActionIdentifier
+        AND DisciplineAction.DisciplineDate = DisciplineActionDiscipline.DisciplineDate
+        AND DisciplineAction.StudentUSI = DisciplineActionDiscipline.StudentUSI
+INNER JOIN edfi.Descriptor
+    ON DisciplineActionDiscipline.DisciplineDescriptorId = Descriptor.DescriptorId
+LEFT JOIN edfi.DisciplineActionStaff
+    ON DisciplineAction.DisciplineActionIdentifier = DisciplineActionStaff.DisciplineActionIdentifier
+        AND DisciplineAction.DisciplineDate = DisciplineActionStaff.DisciplineDate
+        AND DisciplineAction.StudentUSI = DisciplineActionStaff.StudentUSI
+LEFT JOIN edfi.Staff
+    ON DisciplineActionStaff.StaffUSI = Staff.StaffUSI
+WHERE (
         StudentSchoolAssociation.ExitWithdrawDate IS NULL
-        OR StudentSchoolAssociation.ExitWithdrawDate >= GETDATE());
+        OR StudentSchoolAssociation.ExitWithdrawDate >= GETDATE()
+        );
 GO
 
 
--- src/EdFi.AnalyticsMiddleTier.DataStandard32/Equity/PostgreSQL/0003-View-StudentDisciplineActionDim-Create.sql
+++ src/EdFi.AnalyticsMiddleTier.DataStandard32/Equity/PostgreSQL/0003-View-StudentDisciplineActionDim-Create.sql
@@ -2,8 +2,9 @@
 -- Licensed to the Ed-Fi Alliance under one or more agreements.
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
-CREATE OR REPLACE VIEW analytics.equity_StudentDisciplineActionDim
-AS
+CREATE
+    OR REPLACE VIEW analytics.equity_StudentDisciplineActionDim AS
+
 SELECT DISTINCT CONCAT (
         DisciplineAction.DisciplineActionIdentifier
         ,'-'
@@ -18,11 +19,11 @@ SELECT DISTINCT CONCAT (
         ,'-'
         ,StudentSchoolAssociation.SchoolId
         ) AS StudentSchoolKey
-    ,TO_CHAR(DisciplineAction.DisciplineDate, 'yyyymmdd') as DisciplineDateKey
+    ,TO_CHAR(DisciplineAction.DisciplineDate, 'yyyymmdd') AS DisciplineDateKey
     ,Student.StudentUniqueId AS StudentKey
     ,StudentSchoolAssociation.SchoolId AS SchoolKey
     ,Descriptor.Description AS DisciplineActionDescription
-    ,COALESCE(Staff.StaffUniqueId, '') AS  UserKey
+    ,COALESCE(Staff.StaffUniqueId, '') AS UserKey
     ,(
         SELECT MAX(MaxLastModifiedDate)
         FROM (
@@ -32,16 +33,23 @@ SELECT DISTINCT CONCAT (
             ) AS VALUE(MaxLastModifiedDate)
         ) AS LastModifiedDate
 FROM edfi.DisciplineAction
-INNER JOIN edfi.Student ON DisciplineAction.StudentUSI = Student.StudentUSI
-INNER JOIN edfi.StudentSchoolAssociation ON Student.StudentUSI = StudentSchoolAssociation.StudentUSI
-INNER JOIN edfi.DisciplineActionDiscipline ON DisciplineAction.DisciplineActionIdentifier = DisciplineActionDiscipline.DisciplineActionIdentifier
-    AND DisciplineAction.DisciplineDate = DisciplineActionDiscipline.DisciplineDate
-    AND DisciplineAction.StudentUSI = DisciplineActionDiscipline.StudentUSI
-INNER JOIN edfi.Descriptor ON DisciplineActionDiscipline.DisciplineDescriptorId = Descriptor.DescriptorId
-LEFT JOIN edfi.DisciplineActionStaff ON DisciplineAction.DisciplineActionIdentifier = DisciplineActionStaff.DisciplineActionIdentifier
-    AND DisciplineAction.DisciplineDate = DisciplineActionStaff.DisciplineDate
-    AND DisciplineAction.StudentUSI = DisciplineActionStaff.StudentUSI
-LEFT JOIN edfi.Staff ON DisciplineActionStaff.StaffUSI = Staff.StaffUSI
- WHERE(
+INNER JOIN edfi.Student
+    ON DisciplineAction.StudentUSI = Student.StudentUSI
+INNER JOIN edfi.StudentSchoolAssociation
+    ON Student.StudentUSI = StudentSchoolAssociation.StudentUSI
+INNER JOIN edfi.DisciplineActionDiscipline
+    ON DisciplineAction.DisciplineActionIdentifier = DisciplineActionDiscipline.DisciplineActionIdentifier
+        AND DisciplineAction.DisciplineDate = DisciplineActionDiscipline.DisciplineDate
+        AND DisciplineAction.StudentUSI = DisciplineActionDiscipline.StudentUSI
+INNER JOIN edfi.Descriptor
+    ON DisciplineActionDiscipline.DisciplineDescriptorId = Descriptor.DescriptorId
+LEFT JOIN edfi.DisciplineActionStaff
+    ON DisciplineAction.DisciplineActionIdentifier = DisciplineActionStaff.DisciplineActionIdentifier
+        AND DisciplineAction.DisciplineDate = DisciplineActionStaff.DisciplineDate
+        AND DisciplineAction.StudentUSI = DisciplineActionStaff.StudentUSI
+LEFT JOIN edfi.Staff
+    ON DisciplineActionStaff.StaffUSI = Staff.StaffUSI
+WHERE (
         StudentSchoolAssociation.ExitWithdrawDate IS NULL
-        OR StudentSchoolAssociation.ExitWithdrawDate >= NOW());
+        OR StudentSchoolAssociation.ExitWithdrawDate >= NOW()
+        );
