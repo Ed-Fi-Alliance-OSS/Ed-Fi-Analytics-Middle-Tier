b20d069719bfb6639a2f2b1d481034918a3c5e70
Stephen A. Fuqua <stephen@safnet.com>
2021-07-07 12:48:41 -0500

    Default values for GradeBands

--- src/EdFi.AnalyticsMiddleTier.DataStandard32/Engage/MSSQL/0000-Table-GradeBands-Create.sql
+++ src/EdFi.AnalyticsMiddleTier.DataStandard32/Engage/MSSQL/0000-Table-GradeBands-Create.sql
@@ -6,8 +6,18 @@
 CREATE TABLE analytics.engage_GradeBands (
     BandName VARCHAR(15) NOT NULL,
     LowerBound TINYINT NOT NULL,
-    UpperBOund TINYINT NOT NULL,
+    UpperBound TINYINT NOT NULL,
     CONSTRAINT PK_engage_GradeBands PRIMARY KEY CLUSTERED (BandName ASC)
          WITH(PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 )
 ON [PRIMARY];
+
+
+INSERT INTO analytics.engage_GradeBands (BandName, LowerBound, UpperBound)
+VALUES 
+    ('Less than 50%', 0, 50),
+    ('50% to 65%', 50, 65),
+    ('65% to 70%', 65, 70),
+    ('70% to 80%', 70, 80),
+    ('80% to 90%', 80, 90),
+    ('90% to 100%', 90, 100);
