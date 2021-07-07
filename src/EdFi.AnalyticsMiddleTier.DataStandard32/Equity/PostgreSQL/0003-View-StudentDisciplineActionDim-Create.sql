ae08f20d7e82b883c68e33777dd89cca51bdd881
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2021-07-07 12:00:38 -0600

    Fixes the following error: Script block number: 0; Block line 299; Position: 0; Message: 42P16: cannot change name of view column disciplineactionuniquekey to studentdisciplineactionkey.

--- src/EdFi.AnalyticsMiddleTier.DataStandard32/Equity/PostgreSQL/0003-View-StudentDisciplineActionDim-Create.sql
+++ src/EdFi.AnalyticsMiddleTier.DataStandard32/Equity/PostgreSQL/0003-View-StudentDisciplineActionDim-Create.sql
@@ -2,8 +2,11 @@
 -- Licensed to the Ed-Fi Alliance under one or more agreements.
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
-CREATE
-    OR REPLACE VIEW analytics.equity_StudentDisciplineActionDim AS
+
+-- Cannot use `CREATE OR REPLACE VIEW` because a column is being removed
+DROP VIEW IF EXISTS analytics.equity_StudentDisciplineActionDim;
+
+CREATE VIEW analytics.equity_StudentDisciplineActionDim AS
 
 SELECT DISTINCT CONCAT (
         DisciplineAction.DisciplineActionIdentifier
