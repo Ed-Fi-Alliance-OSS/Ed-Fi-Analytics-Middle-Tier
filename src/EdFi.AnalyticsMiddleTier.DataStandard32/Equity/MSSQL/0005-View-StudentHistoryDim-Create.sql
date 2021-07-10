04b1a6b61af0e8822f97488f466ed985ae5059ae
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2021-07-09 18:59:34 -0600

    Replace STRING_AGG
    Update SQL scripts to replace String_AGG. TC has an old SQL Version that does not support STRING_AGG

--- src/EdFi.AnalyticsMiddleTier.DataStandard32/Equity/MSSQL/0005-View-StudentHistoryDim-Create.sql
+++ src/EdFi.AnalyticsMiddleTier.DataStandard32/Equity/MSSQL/0005-View-StudentHistoryDim-Create.sql
@@ -15,6 +15,11 @@ GO
 
 CREATE VIEW analytics.equity_StudentHistoryDim
 AS
+-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
 WITH AttendanceHist
 AS (
     SELECT StudentSchoolKey
@@ -25,57 +30,60 @@ AS (
     )
     ,GradeList
 AS (
-    SELECT DISTINCT studentSectionDim.StudentSectionKey
-        ,STRING_AGG(CAST(CONCAT(studentSectionDim.CourseTitle, ': ', CAST(gradeFact.NumericGradeEarned AS VARCHAR(100))) as VARCHAR(MAX)), CONCAT(', ' , CHAR(10))) AS GradeSummary
-    FROM analytics.ews_StudentSectionGradeFact gradeFact
-    INNER JOIN analytics.StudentSectionDim studentSectionDim
-        ON gradeFact.StudentSectionKey = studentSectionDim.StudentSectionKey
-            AND gradeFact.SectionKey = studentSectionDim.SectionKey
-    GROUP BY studentSectionDim.StudentSectionKey
-        ,gradeFact.GradingPeriodKey
+    SELECT CONCAT(studentSectionDim.StudentKey,'-'
+            ,studentSectionDim.SchoolKey) AS StudentSchoolKey
+            ,LastModifiedDate
+        ,STUFF((
+                SELECT CONCAT (
+                        ', '
+                        ,studentSectionDim.CourseTitle
+                        ,': '
+                        ,grade.NumericGradeEarned
+                        )
+                FROM analytics.ews_StudentSectionGradeFact grade
+                WHERE grade.GradeType IN ('Semester')
+                    AND grade.StudentSectionKey = studentSectionDim.StudentSectionKey
+                    AND grade.SectionKey = studentSectionDim.SectionKey
+                FOR XML PATH('')
+                ), 1, 2, '') AS GradeSummary
+    FROM analytics.StudentSectionDim studentSectionDim
     )
-SELECT DISTINCT studentSchoolDim.StudentKey
-    ,studentSchoolDim.StudentSchoolKey
-    ,STRING_AGG(gradeFact.GradeSummary, CONCAT(', ' , CHAR(10))) AS GradeSummary
-    ,studentSchoolDim.SchoolKey AS CurrentSchoolKey
+SELECT DISTINCT ssd.StudentKey
+    ,ssd.StudentSchoolKey
+    ,CAST(grades.GradeSummary AS VARCHAR(MAX)) as GradeSummary
+    ,ssd.SchoolKey AS CurrentSchoolKey
     ,COALESCE((CAST((DaysEnrolled - DaysAbsent) AS DECIMAL) / CAST(DaysEnrolled AS DECIMAL) * 100), 100) AS AttendanceRate
     ,(
         SELECT COUNT(1)
         FROM analytics.equity_StudentDisciplineActionDim discipline
-        WHERE discipline.StudentSchoolKey = studentSchoolDim.StudentSchoolKey
+        WHERE discipline.StudentSchoolKey = ssd.StudentSchoolKey
         ) AS ReferralsAndSuspensions
-    ,(
-        SELECT STRING_AGG(CONCAT (
-                    SchoolName
+    ,STUFF((
+            SELECT CONCAT (
+                    ','
+                    ,CHAR(10)
+                    ,SchoolName
                     ,' '
-                    ,COALESCE(CAST(ssd.ExitWithdrawDate AS VARCHAR(10)), ' (Current)')
-                    ),CONCAT(', ' , CHAR(10)))
-        FROM edfi.StudentSchoolAssociation ssd
-        INNER JOIN edfi.Student st
-            ON st.StudentUSI = ssd.StudentUSI
-        INNER JOIN analytics.SchoolDim school
-            ON school.SchoolKey = ssd.SchoolId
-        WHERE st.StudentUniqueId = studentSectionDim.StudentKey
-        ) AS EnrollmentHistory
+                    ,COALESCE(CAST(ssa.ExitWithdrawDate AS VARCHAR(10)), ' (Current)')
+                    )
+            FROM edfi.StudentSchoolAssociation ssa
+            INNER JOIN edfi.Student st
+                ON st.StudentUSI = ssa.StudentUSI
+            INNER JOIN analytics.SchoolDim school
+                ON school.SchoolKey = ssa.SchoolId
+            WHERE st.StudentUniqueId = ssd.StudentKey
+            FOR XML PATH('')
+            ), 1, 2, '') AS EnrollmentHistory
     ,(
         SELECT MAX(MaxLastModifiedDate)
         FROM (
-            VALUES (studentSchoolDim.LastModifiedDate)
-                ,(studentSectionDim.LastModifiedDate)
+            VALUES (ssd.LastModifiedDate)
+                ,(grades.LastModifiedDate)
             ) AS VALUE(MaxLastModifiedDate)
         ) AS LastModifiedDate
-FROM analytics.StudentSchoolDim studentSchoolDim
-INNER JOIN analytics.StudentSectionDim studentSectionDim
-    ON StudentSectionDim.StudentKey = studentSchoolDim.StudentKey
-INNER JOIN GradeList gradeFact
-    ON gradeFact.StudentSectionKey = studentSectionDim.StudentSectionKey
+FROM analytics.StudentSchoolDim ssd
+INNER JOIN GradeList grades
+    ON ssd.StudentSchoolKey = grades.StudentSchoolKey
 LEFT OUTER JOIN AttendanceHist ah
-    ON ah.StudentSchoolKey = studentSchoolDim.StudentSchoolKey
-GROUP BY studentSchoolDim.StudentKey
-    ,studentSchoolDim.StudentSchoolKey
-    ,studentSchoolDim.SchoolKey
-    ,DaysEnrolled
-    ,DaysAbsent
-    ,StudentSectionDim.StudentKey
-	,studentSchoolDim.LastModifiedDate
-	,studentSectionDim.LastModifiedDate;
+    ON ah.StudentSchoolKey = ssd.StudentSchoolKey
+WHERE GradeSummary IS NOT NULL;
