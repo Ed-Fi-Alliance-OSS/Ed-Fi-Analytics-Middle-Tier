1bccac032b239e60e29a08d74fea6442f29d44f3
Stephen Fuqua <stephenfuqua@users.noreply.github.com>
2021-07-20 10:04:29 -0500

    [LMS-323] Updates to Core Collection In Prep for Engagement Starter Kit (#62)
    * Changes to SectionDim:
    
    - New SessionKey column
    - New CourseTitle column
    - Fix join and eliminate DISTINCT
    - Rename LocalEducationAgencyId as LocalEducationAgencyKey
    
    * AcademicTimePeriodDim for DS2.2
    
    * Overlooked part of the key
    
    * Copy AcademicTimePeriodDim over to 3.x data standards
    
    * new StudentInternetAccessDim for MSSQL
    
    * Remove SQL Server object qualifiers for easier copying to PostgreSQL
    
    * Copy the new views from MSSQL support
    
    * Restore column `LocalEducationAgencyId`
    
    * CourseOffering join should also have SchoolYear
    Formatmessage does not replace null with '' like concat did
    
    * Restore formatting for easier comparison
    
    * Remove duplicate join condition
    
    * Restore column in PGSQL version
    
    * reformat to match DS31
    
    * rename files to avoid number duplication
    
    * Additional testing around the LastModifiedDate
    
    * Add StudentKey
    Add more test data to prove that joins are working
    
    * Change the tests so that they will actually fail appropriately
    
    * Test now fails when `Session.SchoolYear = GradingPeriod.SchoolYear` is removed, as desired
    
    * Supports proving that join condition `Session.SchoolYear = SessionGradingPeriod.SchoolYear` is necessary
    
    * Testing remainder of join conditions
    Copy recent test changes over to DS2

---
+++ src/EdFi.AnalyticsMiddleTier.Tests/Dimensions/When_querying_the_AcademicTimePeriodDim_view.cs
@@ -0,0 +1,164 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.Diagnostics.CodeAnalysis;
+using EdFi.AnalyticsMiddleTier.Tests.Classes;
+using NUnit.Framework;
+using Shouldly;
+
+namespace EdFi.AnalyticsMiddleTier.Tests.Dimensions
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public abstract class When_querying_the_AcademicTimePeriodDim_view : When_querying_a_view
+    {
+        protected const string TestCasesFolder = "TestCases.AcademicTimePeriodDim";
+
+        protected (bool success, string errorMessage) Result;
+
+        [OneTimeSetUp]
+        public void PrepareDatabase()
+        {
+            DataStandard.PrepareDatabase();
+        }
+
+        [OneTimeSetUp]
+        public void Act()
+        {
+            Result = DataStandard.LoadTestCaseData<GradingPeriodDim>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.0000_AcademicTimePeriodDim_Data_Load.xml");
+            Result.success.ShouldBeTrue($"Error while loading data: '{Result.errorMessage}'");
+
+            Result = DataStandard.Install();
+            Result.success.ShouldBeTrue($"Error while installing Base: '{Result.errorMessage}'");
+        }
+
+        public class Given_default_record_When_querying_the_AcademicTimePeriodDim_view
+        : When_querying_the_AcademicTimePeriodDim_view
+        {
+            public Given_default_record_When_querying_the_AcademicTimePeriodDim_view(TestHarness dataStandard) => SetDataStandard(dataStandard);
+
+            [Test]
+            public void Then_view_should_match_column_dictionary()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<TableColumns>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_match_column_dictionary.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_return_one_record()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_AcademicTimePeriodKey()
+            {
+                
+               (bool success, string errorMessage) testResult = DataStandard.RunTestCase<AcademicTimePeriodDim>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_AcademicTimePeriodKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_GradingPeriodKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<AcademicTimePeriodDim>($"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_GradingPeriodKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_IsCurrentSchoolYear()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<AcademicTimePeriodDim>(
+                        $"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_IsCurrentSchoolYear.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_LastModifiedDate_based_on_GradingPeriod()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<CountResult>(
+                        $"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_LastModifiedDate_based_on_GradingPeriod.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_LastModifiedDate_based_on_Session()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<CountResult>(
+                        $"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_LastModifiedDate_based_on_Session.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_LastModifiedDate_based_on_SchoolYear()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<CountResult>(
+                        $"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_LastModifiedDate_based_on_SchoolYear.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_SchoolKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<AcademicTimePeriodDim>(
+                        $"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_SchoolKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_SchoolYearKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<CountResult>(
+                        $"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_SchoolYearKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_SchoolYearName()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<AcademicTimePeriodDim>(
+                        $"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_SchoolYearName.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_SessionKey()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<AcademicTimePeriodDim>(
+                        $"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_SessionKey.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_should_have_TermName()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<AcademicTimePeriodDim>(
+                        $"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.should_have_TermName.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_additional_join_condition_test()
+            {
+                (bool success, string errorMessage) testResult = DataStandard
+                    .RunTestCase<CountResult>(
+                        $"{TestCasesFolder}.{DataStandard.DataStandardFolderName}.additional_join_condition_test.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+        }
+        
+
+    }
+}
