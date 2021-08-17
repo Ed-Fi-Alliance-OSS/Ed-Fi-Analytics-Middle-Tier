fc3b0cac226192cb2ef27abc85550fd2a50467bf
Luis Richmond <lalrg@outlook.com>
2021-08-17 11:05:07 -0600

    update script format

--- src/EdFi.AnalyticsMiddleTier.DataStandard32/Base/PostgreSQL/0034-View-AcademicTimePeriodDim-Alter.sql
+++ src/EdFi.AnalyticsMiddleTier.DataStandard32/Base/PostgreSQL/0034-View-AcademicTimePeriodDim-Alter.sql
@@ -3,7 +3,9 @@
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
 
-CREATE OR REPLACE VIEW analytics.AcademicTimePeriodDim
+DROP VIEW IF EXISTS analytics.AcademicTimePeriodDim;
+
+CREATE VIEW analytics.AcademicTimePeriodDim
 AS
      SELECT
 		FORMAT(
