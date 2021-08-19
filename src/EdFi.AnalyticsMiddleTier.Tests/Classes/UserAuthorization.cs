a2529aa735200a190adf497a9e5a5e51dd163943
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2021-08-19 10:18:15 -0600

    Add SectionKeyPermission to UserAuthorization to create a relationship with SectionKey (SectionDim).

--- src/EdFi.AnalyticsMiddleTier.Tests/Classes/UserAuthorization.cs
+++ src/EdFi.AnalyticsMiddleTier.Tests/Classes/UserAuthorization.cs
@@ -14,6 +14,7 @@ namespace EdFi.AnalyticsMiddleTier.Tests.Classes
         public string UserScope { get; set; } //(varchar(50), null)
         public string StudentPermission { get; set; } //(varchar(3), not null)
         public string SectionPermission { get; set; } //(varchar(50), null)
+        public string SectionKeyPermission { get; set; }
         public string SchoolPermission { get; set; } //(varchar(30), null)
         public int? DistrictId { get; set; } //(int, null)
     }
