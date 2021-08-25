a9cfde68df7e911a3672e5ce96ac1054b2800e89
Ruben Solano <ruben@nearshoredevs.com>
2021-08-25 08:21:13 -0500

    Adding test cases for postgressql

--- src/EdFi.AnalyticsMiddleTier.Tests/Dimensions/When_querying_the_SchoolDim_view.cs
+++ src/EdFi.AnalyticsMiddleTier.Tests/Dimensions/When_querying_the_SchoolDim_view.cs
@@ -12,7 +12,7 @@ using Shouldly;
 namespace EdFi.AnalyticsMiddleTier.Tests.Dimensions.SchoolDimTestGroup
 {
     [SuppressMessage("ReSharper", "InconsistentNaming")]
-    public abstract class When_querying_the_SchoolDim_view_base : When_querying_a_view
+    public abstract class When_querying_the_SchoolDim_view_base : When_querying_a_view_postgres
     {
         protected const string TestCasesFolder = "TestCases.SchoolDim";
         protected const string TestCasesDataFileName = "0000_SchoolDim_Data_Load.xml";
