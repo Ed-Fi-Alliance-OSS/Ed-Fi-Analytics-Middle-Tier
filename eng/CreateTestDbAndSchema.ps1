bf2c1ed2d0090775285f6b9245835734c6df35b8
Ben Tillman <ben@headspring.com>
2021-09-07 15:50:52 -0500

    Change script logic to drop db if exists then create it (#96)

--- eng/CreateTestDbAndSchema.ps1
+++ eng/CreateTestDbAndSchema.ps1
@@ -3,14 +3,9 @@
 # The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 # See the LICENSE and NOTICES files in the project root for more information.
 
-$dburl="postgresql://postgres@localhost:5432/postgres"
-$data="SELECT datname FROM pg_catalog.pg_database WHERE lower(datname) = lower('EdFi_Ods_Tests');" | psql --csv $dburl | ConvertFrom-Csv
-if ($data.datname -eq $null) {
-    Write-Host "No db found, creating db first"
-    psql -h localhost -p 5432 -U postgres -c "CREATE DATABASE edfi_ods_tests;"
-    Write-Host "Running migration on db"
-    psql -d edfi_ods_tests -h localhost -p 5432 -U postgres -f .\src\EdFi.AnalyticsMiddleTier.Tests\EdFi.Ods.Minimal.Template.sql
-}
-else {
-    Write-Host "db found, not running script"
-}
+Write-Host "dropping db if it exists"
+psql -h localhost -p 5432 -U postgres -c "DROP DATABASE IF EXISTS edfi_ods_tests;"
+Write-Host "creating db"
+psql -h localhost -p 5432 -U postgres -c "CREATE DATABASE edfi_ods_tests;"
+Write-Host "Running migration on db"
+psql -d edfi_ods_tests -h localhost -p 5432 -U postgres -f .\src\EdFi.AnalyticsMiddleTier.Tests\EdFi.Ods.Minimal.Template.sql
