ca317c7f5f9e27eabad65b8bb294c35787d0296f
Ben Tillman <ben@headspring.com>
2021-10-08 15:46:35 -0500

    [BIA-848] Add Postgres tests: StudentProgramDim View (#127)

--- src/EdFi.AnalyticsMiddleTier.Tests/Dimensions/When_querying_the_StudentProgramDim_view.cs
+++ src/EdFi.AnalyticsMiddleTier.Tests/Dimensions/When_querying_the_StudentProgramDim_view.cs
@@ -7,7 +7,7 @@ using Shouldly;
 namespace EdFi.AnalyticsMiddleTier.Tests.Dimensions.StudentProgramDimTestGroup
 {
     [SuppressMessage("ReSharper", "InconsistentNaming")]
-    public abstract class When_querying_the_StudentProgramDim_view : When_querying_a_view
+    public abstract class When_querying_the_StudentProgramDim_view : When_querying_a_view_postgres
     {
 
         protected const string TestCasesFolder = "TestCases.StudentProgramDim";
