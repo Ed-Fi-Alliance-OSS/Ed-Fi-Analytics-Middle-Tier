cfee86e92361cef0c82d9c7c8b32f6ee046dafaf
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-03-09 11:44:30 -0600

    Renamed the view to include the Dim suffix.

--- src/EdFi.AnalyticsMiddleTier.DataStandard33/EPP/MSSQL/0000-View-TeacherCandidateMainDim-Create.sql
+++ src/EdFi.AnalyticsMiddleTier.DataStandard33/EPP/MSSQL/0000-View-TeacherCandidateMainDim-Create.sql
@@ -2,16 +2,28 @@
 -- Licensed to the Ed-Fi Alliance under one or more agreements.
 -- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
 -- See the LICENSE and NOTICES files in the project root for more information.
-DROP VIEW IF EXISTS analytics.tpdm_TeacherCandidateMain;
 
-CREATE VIEW analytics.tpdm_TeacherCandidateMain AS
-	SELECT c.CandidateIdentifier
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('analytics.tpdm_TeacherCandidateMainDim') IS NOT NULL 
+	DROP VIEW analytics.tpdm_TeacherCandidateMainDim
+
+GO
+
+
+CREATE VIEW analytics.tpdm_TeacherCandidateMainDim AS
+
+SELECT c.CandidateIdentifier
 		,c.FirstName
 		,c.LastSurname
 		,c.SexDescriptorId
 		,COALESCE(rd.RaceDescriptorId, 0) AS RaceDescriptorId
-		,COALESCE(c.HispanicLatinoEthnicity, false) AS HispanicLatinoEthnicity
-		,COALESCE(c.EconomicDisadvantaged, false) AS EconomicDisadvantaged
+		,COALESCE(c.HispanicLatinoEthnicity, 0) AS HispanicLatinoEthnicity
+		,COALESCE(c.EconomicDisadvantaged, 0) AS EconomicDisadvantaged
 		,COALESCE(ccy.SchoolYear, 0) AS Cohort
 		,CAST(CASE WHEN red.CodeValue = 'Received certificate of completion or equivalent' THEN 1 ELSE 0 END AS BIT) ProgramComplete
 		,COALESCE(s.StudentUSI, 0) AS StudentUSI
@@ -19,7 +31,7 @@ CREATE VIEW analytics.tpdm_TeacherCandidateMain AS
 		,epp.BeginDate
 		,epp.EducationOrganizationId
 		,COALESCE(c.PersonId, '') AS PersonId
-		,COALESCE(CASE WHEN SUM(CASE WHEN cred.CredentialIdentifier IS NOT NULL THEN 1 ELSE 0 END) > 0 THEN CAST(MIN(cred.IssuanceDate) as VARCHAR) END, '') IssuanceDate
+		,COALESCE(CASE WHEN SUM(CASE WHEN cred.CredentialIdentifier IS NOT NULL THEN 1 ELSE 0 END) > 0 THEN CAST(MIN(cred.IssuanceDate) as NVARCHAR) END, '') IssuanceDate
         ,COALESCE(termdesc.CodeValue, '') AS CohortYearTermDescription
 	FROM tpdm.Candidate c 
 	JOIN tpdm.CandidateEducatorPreparationProgramAssociation epp ON epp.CandidateIdentifier = c.CandidateIdentifier 
@@ -47,4 +59,5 @@ CREATE VIEW analytics.tpdm_TeacherCandidateMain AS
 		,epp.BeginDate 
 		,epp.EducationOrganizationId 
 		,c.PersonId
-        ,termdesc.CodeValue;
+        ,termdesc.CodeValue
+GO
