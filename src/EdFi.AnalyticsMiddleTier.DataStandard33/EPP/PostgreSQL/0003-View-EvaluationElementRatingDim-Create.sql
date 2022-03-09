21a67df7e091ff18f631dcb66136c5053b919d19
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-03-09 16:06:36 -0600

    Adds EPP_EvaluationElementRatingDim to EPP collection.

---
+++ src/EdFi.AnalyticsMiddleTier.DataStandard33/EPP/PostgreSQL/0003-View-EvaluationElementRatingDim-Create.sql
@@ -0,0 +1,23 @@
+﻿-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+DROP VIEW IF EXISTS analytics.EPP_CandidateDim;
+
+CREATE VIEW analytics.EPP_EvaluationElementRatingDim AS
+---Evaluation Rating: perfomance evaluation >> Objective >> Element
+SELECT 
+	DISTINCT Candidate.CandidateIdentifier
+		,EvaluationElementRatingResult.EvaluationDate
+		,EvaluationElementRatingResult.PerformanceEvaluationTitle
+		,EvaluationObjective.EvaluationObjectiveTitle
+		,EvaluationElementRatingResult.EvaluationElementTitle
+		,EvaluationElementRatingResult.RatingResultTitle
+		,EvaluationElementRatingResult.Rating
+FROM
+	tpdm.EvaluationElementRatingResult
+	JOIN tpdm.Candidate
+		ON EvaluationElementRatingResult.PersonId = Candidate.PersonId
+	JOIN tpdm.EvaluationObjective
+		ON EvaluationElementRatingResult.EvaluationObjectiveTitle = EvaluationObjective.EvaluationObjectiveTitle;
---
+++ src/EdFi.AnalyticsMiddleTier.Tests/Dimensions/When_querying_the_EvaluationElementRatingDim_view.cs
@@ -0,0 +1,100 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System.Diagnostics.CodeAnalysis;
+using EdFi.AnalyticsMiddleTier.Tests.Classes;
+using EdFi.AnalyticsMiddleTier.Common;
+using NUnit.Framework;
+using Shouldly;
+
+// ReSharper disable once CheckNamespace
+namespace EdFi.AnalyticsMiddleTier.Tests.Dimensions.EvaluationElementRatingDimTestGroup
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public abstract class When_querying_the_EvaluationElementRatingDim_View : When_querying_a_view_ds3_3
+    {
+        protected const string TestCasesFolder = "TestCases.EvaluationElementRatingDim";
+        protected const string TestCasesDataFileName = "0000_EvaluationElementRatingDim_Data_Load.xml";
+
+        [Test]
+        public void Then_view_should_match_column_dictionary()
+        {
+            (bool success, string errorMessage) testResult = DataStandard.RunTestCase<TableColumns>($"{TestCasesFolder}.{DataStandard.TestDataFolderName}.0001_view_should_match_column_dictionary.xml");
+            testResult.success.ShouldBe(true, testResult.errorMessage);
+        }
+        
+        [SetUpFixture]
+        public class SetupEvaluationElementRatingDimTestCase
+            : When_querying_the_EvaluationElementRatingDim_View
+        {
+            [OneTimeSetUp]
+            public void PrepareDatabase() => PrepareTestData<EvaluationElementRatingDim>(TestCasesFolder, TestCasesDataFileName, Component.EPP);
+        }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        public class Given_candidateidentifier_1000048
+        : When_querying_the_EvaluationElementRatingDim_View
+        {
+            public Given_candidateidentifier_1000048(TestHarnessBase dataStandard) => SetDataStandard(dataStandard);
+            private string _caseIdentifier = "c1000048";
+
+            [Test]
+            public void Then_should_return_one_record()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<CountResult>($"{TestCasesFolder}.{_caseIdentifier}_should_return_one_record.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_CandidateIdentifier_should_be_1000048()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<EvaluationElementRatingDim>($"{TestCasesFolder}.{_caseIdentifier}_CandidateIdentifier_should_be_1000048.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_EvaluationDate_should_be_2011_11_01()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<EvaluationElementRatingDim>($"{TestCasesFolder}.{_caseIdentifier}_EvaluationDate_should_be_2011_11_01.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_EvaluationElementTitle_should_be_adddress_misconceptions()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<EvaluationElementRatingDim>($"{TestCasesFolder}.{_caseIdentifier}_EvaluationElementTitle_should_be_adddress_misconceptions.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_EvaluationObjectiveTitle_should_be_adddress_misconceptions()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<EvaluationElementRatingDim>($"{TestCasesFolder}.{_caseIdentifier}_EvaluationObjectiveTitle_should_be_adddress_misconceptions.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_PerformanceEvaluationTitle_should_be_Formal_Evaluation()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<EvaluationElementRatingDim>($"{TestCasesFolder}.{_caseIdentifier}_PerformanceEvaluationTitle_should_be_Formal_Evaluation.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_Rating_should_be_61()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<EvaluationElementRatingDim>($"{TestCasesFolder}.{_caseIdentifier}_Rating_should_be_61.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+
+            [Test]
+            public void Then_RatingResultTitle_should_be_Effective()
+            {
+                (bool success, string errorMessage) testResult = DataStandard.RunTestCase<EvaluationElementRatingDim>($"{TestCasesFolder}.{_caseIdentifier}_RatingResultTitle_should_be_Effective.xml");
+                testResult.success.ShouldBe(true, testResult.errorMessage);
+            }
+        }
+    }
+}
