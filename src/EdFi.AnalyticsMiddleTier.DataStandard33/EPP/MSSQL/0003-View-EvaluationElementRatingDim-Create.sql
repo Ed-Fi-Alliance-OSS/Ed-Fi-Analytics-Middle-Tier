b9cebf04489de48fb727109c21084fd414ae829d
David Jimenez Barrantes <djimenez@growthaccelerationpartners.com>
2022-03-14 16:49:47 -0600

    [BIA-1009] Adds EPP_EvaluationElementRatingDim to EPP collection. (#189)

---
+++ src/EdFi.AnalyticsMiddleTier.DataStandard33/EPP/MSSQL/0003-View-EvaluationElementRatingDim-Create.sql
@@ -0,0 +1,39 @@
+﻿-- SPDX-License-Identifier: Apache-2.0
+-- Licensed to the Ed-Fi Alliance under one or more agreements.
+-- The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+-- See the LICENSE and NOTICES files in the project root for more information.
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+IF OBJECT_ID('analytics.EPP_EvaluationElementRatingDim') IS NOT NULL
+	DROP VIEW analytics.EPP_EvaluationElementRatingDim
+
+GO
+
+CREATE VIEW analytics.EPP_EvaluationElementRatingDim AS
+
+---Evaluation Rating: perfomance evaluation >> Objective >> Element
+SELECT 
+	DISTINCT Candidate.CandidateIdentifier AS CandidateKey
+		,EvaluationElementRatingResult.EvaluationDate
+		,EvaluationElementRatingResult.PerformanceEvaluationTitle
+		,EvaluationObjective.EvaluationObjectiveTitle
+		,EvaluationElementRatingResult.EvaluationElementTitle
+		,EvaluationElementRatingResult.RatingResultTitle
+		,EvaluationElementRatingResult.Rating,
+		(	SELECT 
+				MAX(MaxLastModifiedDate)
+			FROM (VALUES (Candidate.LastModifiedDate)
+						,(EvaluationObjective.LastModifiedDate)
+				 ) AS VALUE (MaxLastModifiedDate)
+		) AS LastModifiedDate
+FROM
+	tpdm.EvaluationElementRatingResult
+	JOIN tpdm.Candidate
+		ON EvaluationElementRatingResult.PersonId = Candidate.PersonId
+	JOIN tpdm.EvaluationObjective
+		ON EvaluationElementRatingResult.EvaluationObjectiveTitle = EvaluationObjective.EvaluationObjectiveTitle;
---
+++ src/EdFi.AnalyticsMiddleTier.Tests/Classes/EvaluationElementRatingDim.cs
@@ -0,0 +1,23 @@
+﻿// SPDX-License-Identifier: Apache-2.0
+// Licensed to the Ed-Fi Alliance under one or more agreements.
+// The Ed-Fi Alliance licenses this file to you under the Apache License, Version 2.0.
+// See the LICENSE and NOTICES files in the project root for more information.
+
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace EdFi.AnalyticsMiddleTier.Tests.Classes
+{
+    [SuppressMessage("ReSharper", "UnusedMember.Global")]
+    public class EvaluationElementRatingDim
+    {
+        public string CandidateKey { get; set; }
+        public DateTime EvaluationDate { get; set; }
+        public string PerformanceEvaluationTitle { get; set; }
+        public string EvaluationObjectiveTitle { get; set; }
+        public string EvaluationElementTitle { get; set; }
+        public string RatingResultTitle { get; set; }
+        public decimal Rating { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+    }
+}
