1b4cf972ef88ff925f97696436b1de01e7c5da0b
José Alberto Leiva Obando <jleiva@growthaccelerationpartners.com>
2022-03-23 13:36:12 -0600

    Remove coalesce for LastName column

--- src/EdFi.AnalyticsMiddleTier.DataStandard32/Base/PostgreSQL/0043-View-AllStudentSchoolDim-Create.sql
+++ src/EdFi.AnalyticsMiddleTier.DataStandard32/Base/PostgreSQL/0043-View-AllStudentSchoolDim-Create.sql
@@ -23,7 +23,7 @@ CREATE OR REPLACE VIEW analytics.AllStudentSchoolDim AS
         ,COALESCE(CAST(StudentSchoolAssociation.SchoolYear AS VARCHAR), 'Unknown') AS SchoolYear
         ,Student.FirstName AS StudentFirstName
         ,COALESCE(Student.MiddleName, '') AS StudentMiddleName
-        ,COALESCE(Student.LastSurname, '') AS StudentLastName
+        ,Student.LastSurname AS StudentLastName
         ,CAST(StudentSchoolAssociation.EntryDate AS VARCHAR) AS EnrollmentDateKey
         ,Descriptor.CodeValue AS GradeLevel
         ,COALESCE(CASE 
